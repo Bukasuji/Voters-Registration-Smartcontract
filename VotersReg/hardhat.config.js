4353715af85faa2ff86a534a7402f387c914b04f
Chukwuebuka Osuji <74817012+Bukasuji@users.noreply.github.com>
2022-11-05 13:53:17 +0530

    added etherscan api

--- VotersReg/hardhat.config.js
+++ VotersReg/hardhat.config.js
@@ -1,8 +1,10 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config({ path: ".env" });
+require("@nomiclabs/hardhat-etherscan");
 
 const QUICKNODE_HTTP_URL = process.env.QUICKNODE_HTTP_URL;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
 
 module.exports = {
   solidity: "0.8.9",
@@ -12,4 +14,9 @@ module.exports = {
       accounts: [PRIVATE_KEY],
     },
   },
+  etherscan: {
+    // Your API key for Etherscan
+    // Obtain one at https://etherscan.io/
+    apiKey: ETHERSCAN_API_KEY
+  }
 };
